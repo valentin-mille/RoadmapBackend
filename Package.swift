f6d588d4053f4ca8a6172f4e39b96b040a728813
Mille <valentin.mille@epitech.eu>
2023-09-11 07:57:17 +0200

    first commit

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.8
+import PackageDescription
+
+let package = Package(
+    name: "RoadmapBackend",
+    platforms: [
+       .macOS(.v13)
+    ],
+    dependencies: [
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.77.1"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "App",
+            dependencies: [
+                .product(name: "Vapor", package: "vapor")
+            ]
+        ),
+        .testTarget(name: "AppTests", dependencies: [
+            .target(name: "App"),
+            .product(name: "XCTVapor", package: "vapor"),
+        ])
+    ]
+)
