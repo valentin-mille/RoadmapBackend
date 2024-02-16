5b94b6dcb508c69ab556228d0bf08ba2db5313ba
Mille <valentin.mille@epitech.eu>
2024-02-16 14:33:53 +0100

    feat(configure): load backend with db url instead

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -4,25 +4,15 @@ import Fluent
 
 // configures your application
 public func configure(_ app: Application) async throws {
-    guard let host = Environment.get("DATABASE_HOST"),
-          let username = Environment.get("DATABASE_USERNAME"),
-          let password = Environment.get("DATABASE_PASSWORD")
+    guard let databaseURL = Environment.get("DATABASE_URL")
     else {
         app.shutdown()
         print("Could not load variable environment. Shutting down the app.")
         return
     }
 
-    let configuration = SQLPostgresConfiguration(
-        hostname: host,
-        username: username,
-        password: password,
-        tls: .disable
-    )
-    app.databases.use(.postgres(configuration: configuration, decodingContext: .default), as: .psql)
-
+    try app.databases.use(.postgres(url: databaseURL), as: .psql)
     app.migrations.add(RoadmapFeature.Create())
-
     app.logger.logLevel = .debug
 
     try await app.autoMigrate()
